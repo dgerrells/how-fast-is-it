02814211d96df9195969f213eb98d2fc6e9fc1d7
David Gerrells <david@davidgerrells.com>
2025-09-08 22:29:49 -0700

    drop useless permission set

--- setup.sh
+++ setup.sh
@@ -54,9 +54,6 @@ configure_caddy_file() {
     echo "Copying $caddyfile to /etc/caddy/ and restarting Caddy."
     sudo mkdir -p /etc/caddy
     sudo cp "$caddyfile" /etc/caddy/
-    echo "Setting Caddy permissions to bind to low ports..."
-    sudo setcap cap_net_bind_service=+ep /usr/bin/caddy
-    echo "Caddy permissions configured."
     sudo systemctl restart caddy
     echo "Caddy service reloaded with new Caddyfile."
 }
